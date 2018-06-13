9e8050fd7e8f726603407607d2c54555c7a40421
cyrustang <chiuyat88@gmail.com>
2018-06-13 08:08:04 +0100

    update readme for run instruction fix youtube down utils

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ This is a Django project to run a karaoke server at home!
 
 How to run
 ===
-docker built -t myweb .
-docker run --rm -it -d -p 127.0.0.1:11080:11080 myweb
+git pull && docker build -t myweb:latest .
+docker kill home
+docker run --name home --rm -it -d -p 11080:11080 myweb
+
+
+Tips
+===
+To run shell in alpine use /bin/ash
+Use apk instead of apt-get for package management
--- apps/khome/utils.py
+++ apps/khome/utils.py
@@ -1,11 +1,11 @@
 import youtube_dl
-
+import os
 
 def download_youtube(dir, video_id):
     """ store the {id}.mp4 and {id}.mp3"""
     ydl_opts = {
         'format': 'mp4',
-        'outtmpl': dir + '/%(id)s.%(ext)s',
+        'outtmpl': os.path.join(dir ,'/%(id)s.%(ext)s'),
         'keepvideo': True,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -20,4 +20,6 @@ def download_youtube(dir, video_id):
 
 if __name__=='__main__':
     id = 'L6joGUdc6y4'
-    download_youtube(id)
+    dir = "tmp"
+    os.mkdir(dir)
+    download_youtube(dir, id)
