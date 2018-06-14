32529d4a85c4dcb9bf217d33f3bd5ed4291a3ad7
cyrustang <chiuyat88@gmail.com>
2018-06-14 01:09:32 +0100

    adding control page

--- apps/khome/urls.py
+++ apps/khome/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('control', views.control, name='control'),
+    path('search/<query>', views.search, name='search')
 ]
--- apps/khome/views.py
+++ apps/khome/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from collections import deque
 
 # Create your views here.
+playlist = deque()
+state = { 'vocal': False,
+          'play': True,
+          'skip': True}
+
+def control(request):
+    template = loader.get_template("khome/control.html")
+    context = { 'playlist': playlist }
+    return HttpResponse(template.render(context))
+
+def search(request, query):
+    return HttpResponse()
