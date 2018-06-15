ce118900ea8860c8651a1be1a017e55ec63e7b1d
cyrustang <chiuyat88@gmail.com>
2018-06-15 09:04:06 +0100

    adding play media functions

--- apps/khome/urls.py
+++ apps/khome/urls.py
@@ -3,13 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('play/<video_id>', views.khome, name='play'),
+
+    path('control', views.control, name='control'),
+    path('search/<query>', views.search, name='search'),
+    path('download/<video_id>/<video_title>', views.download, name='download'),
+
 
     path('state', views.check_state, name='state'),
     path('toggle/<param>', views.toggle, name='toggle'),
     path('playlist', views.show_playlist, name='playlist'),
     path('popPlaylist', views.pop_playlist, name='pop'),
-
-    path('control', views.control, name='control'),
-    path('search/<query>', views.search, name='search'),
 
 ]
--- apps/khome/views.py
+++ apps/khome/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, JsonResponse
+from django.conf import settings
 from collections import deque
+from .utils import search_youtube,download_youtube, remove_vocal, Result
 
 # Create your views here.
 playlist = deque()
@@ -9,13 +11,33 @@ state = { 'vocal': False,
           'play': True,
           'skip': True}
 
+def index(request):
+    template = loader.get_template("khome/index.html")
+    context = { 'playlist': playlist,
+                'control_page': request.build_absolute_uri('control') }
+    return HttpResponse(template.render(context))
+
+def khome(request, video_id):
+    template = loader.get_template("khome/play.html")
+    context = { "video_id" : video_id }
+    return HttpResponse(template.render(context))
+
 def control(request):
     template = loader.get_template("khome/control.html")
     context = { 'playlist': playlist }
     return HttpResponse(template.render(context))
 
 def search(request, query):
-    return HttpResponse()
+    results = search_youtube(query)
+    template = loader.get_template("khome/search_result.html")
+    context = {"items" : results}
+    return HttpResponse(template.render(context))
+
+def download(request, video_id, video_title):
+    download_youtube(settings.MEDIA_ROOT, video_id)
+    remove_vocal(f'{settings.MEDIA_ROOT}/{video_id}.mp3', f'{settings.MEDIA_ROOT}/{video_id}-music.mp3')
+    playlist.append(Result(video_id, video_title))
+    return HttpResponse("Success: " + video_id)
 
 def toggle(request, param):
     if param not in state.keys():
