b7a8db56f732344f54708abff6a350b3eb06c546
cyrustang <chiuyat88@gmail.com>
2018-06-16 22:30:30 +0100

    refactor ajax call

--- apps/khome/static/khome/common.js
+++ apps/khome/static/khome/common.js
@@ -1,18 +1,18 @@
 //ref: https://codereview.stackexchange.com/questions/121802/a-reusable-ajax-polling-function
 
-function Request() {
+function Poll() {
   this.pollTimer = null;
   this.interval = 1000;
   this.url = './request.php';
   this.func = response => console.log(response)
 }
 
-Request.prototype.disablePoll = function () {
+Poll.prototype.disablePoll = function () {
   clearInterval(this.pollTimer);
   this.pollTimer = null;
 };
 
-Request.prototype.activatePoll = function () {
+Poll.prototype.activatePoll = function () {
   this.pollTimer = setInterval(() => {
     $.getJSON(this.url).then(this.func)
   }, this.interval);
